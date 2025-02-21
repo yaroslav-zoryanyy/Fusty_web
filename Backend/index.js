bb27d9cb381964fbfa1ae6585938fef38cb5c9d7
yaroslav <nemobroun@gmail.com>
2025-02-21 22:15:33 +0200

    The primary base of the data and the relationship between it

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,25 @@
 import dotenv from 'dotenv'
 import express from 'express';
+import sequelize from './db.js';
+import module from './models/models.js';
+import cors from 'cors';
+
+const PORT = process.env.PORT|| 5000;
 
 const app = express();
 dotenv.config()
 
-const PORT = process.env.PORT|| 5000;
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+   try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+       app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+start()
---
+++ Backend/models/models.js
@@ -0,0 +1,64 @@
+import sequelize from "../db.js"
+import {DataTypes} from "sequelize"
+
+const User = sequelize.define("user", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    phone:{type:DataTypes.STRING, unique: true},
+    password:{type:DataTypes.STRING, allowNull: false},
+    role:{type:DataTypes.STRING, defaultValue: 'USER'},
+})
+
+const Basket = sequelize.define("basket", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+})
+
+const BasketProduct = sequelize.define("basket_product", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+})
+
+const Product = sequelize.define("product", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    name:{type:DataTypes.STRING, unique: true, allowNull: false},
+    price:{type:DataTypes.INTEGER, allowNull: false },
+    img:{type:DataTypes.INTEGER, defaultValue: 0},
+    rating:{type:DataTypes.STRING, allowNull: false},
+})
+
+const Services = sequelize.define("services", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    type:{type:DataTypes.STRING,unique: true, allowNull: false},
+})
+
+const Rating = sequelize.define("rating", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    rate:{type:DataTypes.INTEGER, allowNull: false},
+})
+
+const ProductInfo = sequelize.define("product_info", {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement: true},
+    title:{type:DataTypes.STRING, allowNull: false},
+    description:{type:DataTypes.STRING, allowNull: false},
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketProduct)
+BasketProduct.belongsTo(Basket)
+
+Services.hasMany(Product)
+Product.belongsTo(Services)
+
+Product.hasMany(Rating)
+Rating.belongsTo(Product)
+
+Product.hasMany(BasketProduct)
+BasketProduct.belongsTo(Product)
+
+Product.belongsTo(ProductInfo)
+ProductInfo.belongsTo(Product)
+
+export default {User, Basket, Services, Rating, ProductInfo,Product,BasketProduct};
